52628fda715a566df1226c6c223d6fb001064e9c
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-10-19 21:34:56 +0500

    Добавьте файлы проекта.

---
+++ Regex_n_LINQ/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Regex_n_LINQ
+{
+    public class File
+    {
+        private string[] _path_001 = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Input*.txt");
+        private string _path_002 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Pattern.txt";
+        public List<string> ContentFiles = new List<string>();
+        public void GetContentFiles()
+        {
+            
+            for (int i = 0; i < _path_001.Length; i++)
+            {
+                ContentFiles.Add(_path_001[i]);
+            }
+
+            
+            foreach (string file in ContentFiles)
+            {
+                Console.WriteLine(file + "\n");
+            }
+            
+        }
+    }
+    public class Class1
+    {
+        public MatchCollection myRegex(string input, string pattern, string replacement = "Заменено")
+        {
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            Console.WriteLine(Regex.Replace(input, pattern, replacement));
+            return matches;
+        }
+        public void PrintMatches(MatchCollection matches)
+        {
+            foreach (var item in matches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Regex_n_LINQ/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Regex_n_LINQ
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            //string text, pattern001, pattern002 = "";
+            //try
+            //{
+            //    text = args[0];
+            //    pattern001 = args[1];
+            //    try
+            //    {
+            //        pattern002 = args[2];
+            //    }
+            //    catch
+            //    {
+
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Введите текст:");
+            //    text = Console.ReadLine();
+            //    Console.WriteLine("Введите шаблон поиска (RegEx):");
+            //    pattern001 = Console.ReadLine();
+            //}
+            //Class1 class1 = new Class1();
+            //class1.PrintMatches(class1.myRegex(text, pattern001));
+            //if (pattern002 != "")
+            //{
+            //    class1.PrintMatches(class1.myRegex(text, pattern002));
+            //}
+            File f = new File();
+            f.GetContentFiles();
+        }
+    }
+}
